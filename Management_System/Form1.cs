57c01e9a8f534ab293dbe8bb1afd32f553ed21bd
nellydiyanova <Emily_5@abv.bg>
2022-06-08 18:39:48 +0300

    Създаване на първа форма - логин. Създаване на базата данни, свързване и създаване на първата таблица със запис.

--- Management_System/Form1.cs
+++ Management_System/Form1.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.SqlClient;
 using System.Windows.Forms;
 
 namespace Management_System
@@ -15,6 +10,54 @@ namespace Management_System
         public Form1()
         {
             InitializeComponent();
+        }
+
+
+        public string cs = @"Data Source = (LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\User\source\repos\Management_System\Management_System\DB_System.mdf;Integrated Security = True";
+        public SqlConnection myconnection = default(SqlConnection);
+        public SqlCommand mycommand = default(SqlCommand);
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myconnection = new SqlConnection(cs);
+                mycommand = new SqlCommand("Select * from Users where username=@username and password=@password", myconnection);
+                SqlParameter uUsername = new SqlParameter("@username", SqlDbType.VarChar);
+                SqlParameter uPassword = new SqlParameter("@password", SqlDbType.VarChar);
+                uUsername.Value = textBox1.Text;
+                uPassword.Value = textBox2.Text;
+                mycommand.Parameters.Add(uUsername);
+                mycommand.Parameters.Add(uPassword);
+                mycommand.Connection.Open();
+                SqlDataReader myreader = mycommand.ExecuteReader(CommandBehavior.CloseConnection);
+                if (myreader.Read() == true)
+                {
+                    MessageBox.Show("Добре дошли, " + textBox1.Text + "!");
+                    this.Hide();
+                    //Form2 frm = new Form2();
+                    //frm.Show();
+
+                }
+                else
+                {
+                    MessageBox.Show("Невалидни данни!", "Достъп отказан", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    textBox1.Clear();
+                    textBox2.Clear();
+                    textBox1.Focus();
+                }
+
+                if (myconnection.State == ConnectionState.Open)
+                {
+                    myconnection.Dispose();
+                }
+
+            }
+            catch (ExecutionEngineException ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
         }
     }
 }
