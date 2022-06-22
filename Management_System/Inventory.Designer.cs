ef5f471ca30a1c7c223b6d10380fcadbfa31eaa3
nellydiyanova <Emily_5@abv.bg>
2022-06-22 21:05:37 +0300

    Въвеждане на нова стока и добавяне към базата данни и treeview. При маркиран склад или артикул, излизат данните за него в празните полета. Сложени икони към trieview.

--- Management_System/Inventory.Designer.cs
+++ Management_System/Inventory.Designer.cs
@@ -29,6 +29,7 @@ namespace Management_System
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Inventory");
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Inventory));
             this.treeView1 = new System.Windows.Forms.TreeView();
@@ -47,8 +48,26 @@ namespace Management_System
             this.label4 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.menuStrip1.SuspendLayout();
             this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // treeView1
@@ -61,7 +80,7 @@ namespace Management_System
             treeNode1.Text = "Inventory";
             this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
             treeNode1});
-            this.treeView1.Size = new System.Drawing.Size(236, 400);
+            this.treeView1.Size = new System.Drawing.Size(236, 429);
             this.treeView1.TabIndex = 0;
             this.treeView1.NodeMouseClick += new System.Windows.Forms.TreeNodeMouseClickEventHandler(this.treeView1_NodeMouseClick);
             // 
@@ -92,6 +111,7 @@ namespace Management_System
             this.новаСтокаToolStripMenuItem.Name = "новаСтокаToolStripMenuItem";
             this.новаСтокаToolStripMenuItem.Size = new System.Drawing.Size(87, 23);
             this.новаСтокаToolStripMenuItem.Text = "Нова стока";
+            this.новаСтокаToolStripMenuItem.Click += new System.EventHandler(this.новаСтокаToolStripMenuItem_Click);
             // 
             // стоковаСправкаToolStripMenuItem
             // 
@@ -200,12 +220,171 @@ namespace Management_System
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(3, 45);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 19);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Стока:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(5, 86);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(100, 19);
+            this.label8.TabIndex = 12;
+            this.label8.Text = "Доставна цена:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(5, 118);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(103, 19);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Продажна цена:";
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(282, 102);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(66, 26);
+            this.textBox10.TabIndex = 17;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(56, 7);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 26);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(263, 10);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(99, 26);
+            this.textBox6.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(7, 10);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 19);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "ID:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 13);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 19);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Склад:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(55, 42);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(307, 26);
+            this.textBox7.TabIndex = 13;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(111, 83);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(82, 26);
+            this.textBox8.TabIndex = 13;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(111, 115);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(82, 26);
+            this.textBox9.TabIndex = 15;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(226, 105);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(50, 19);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Мярка:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label11);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox10);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.textBox9);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.textBox8);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.textBox6);
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Location = new System.Drawing.Point(241, 200);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(372, 229);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Visible = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(199, 150);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 27);
+            this.comboBox1.TabIndex = 19;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(107, 153);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(74, 19);
+            this.label11.TabIndex = 18;
+            this.label11.Text = "Доставчик:";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button2.Location = new System.Drawing.Point(135, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(98, 35);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Създай";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "Warehouse 2.png");
+            this.imageList1.Images.SetKeyName(1, "Book.png");
+            this.imageList1.Images.SetKeyName(2, "Inventory.png");
+            // 
             // Inventory
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-            this.ClientSize = new System.Drawing.Size(625, 400);
+            this.ClientSize = new System.Drawing.Size(625, 429);
+            this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.menuStrip1);
             this.Controls.Add(this.treeView1);
@@ -220,6 +399,8 @@ namespace Management_System
             this.menuStrip1.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,5 +424,22 @@ namespace Management_System
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ImageList imageList1;
     }
 }
--- Management_System/Inventory.cs
+++ Management_System/Inventory.cs
@@ -34,6 +34,7 @@ namespace Management_System
 
         private void displayData2()
         {
+
             myConnection.Open();
             adapt = new SqlDataAdapter("Select warehouse from Warehouses", myConnection);
             adapt.Fill(dt1);
@@ -41,13 +42,33 @@ namespace Management_System
             {
                 TreeNode parentnode = new TreeNode(dr["warehouse"].ToString());
                 treeView1.Nodes.Add(parentnode);
+                treeView1.ImageList = imageList1;
+                treeView1.ImageIndex = 0;
             }
+            myConnection.Close();
+        }
+
+        private void displayData3()
+        {
+            myConnection.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("Select * from Warehouses where warehouse='" + treeView1.SelectedNode + "'", myConnection);
+            myConnection.Close();
+        }
+
+        private void displayData4()
+        {
+            myConnection.Open();
+            DataTable dt = new DataTable();
+            adapt = new SqlDataAdapter("Select * from Inventory where product_name='" + treeView1.SelectedNode + "'", myConnection);
             myConnection.Close();
         }
 
         private void Inventory_Load(object sender, EventArgs e)
         {
             groupBox1.Visible = false;
+            groupBox2.Visible = false;
+
             try
             {
                 myConnection = new SqlConnection(frm.cs);
@@ -73,11 +94,74 @@ namespace Management_System
         private void новСкладToolStripMenuItem_Click(object sender, EventArgs e)
         {
             groupBox1.Visible = true;
+            textBox1.Focus();
         }
 
         private void treeView1_NodeMouseClick(object sender, TreeNodeMouseClickEventArgs e)
         {
-            groupBox1.Visible = true;
+            this.treeView1.SelectedNode = this.treeView1.Nodes[0];
+            if (this.treeView1.Nodes[0].IsSelected)
+            {
+                textBox1.Clear();
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+                textBox6.Clear();
+                textBox7.Clear();
+                textBox8.Clear();
+                textBox9.Clear();
+                textBox10.Clear();
+                comboBox1.Text = "";
+                groupBox1.Visible = false;
+                groupBox2.Visible = false;
+            }
+
+                groupBox1.Visible = true;
+                myConnection = new SqlConnection(frm.cs);
+                myCommand = new SqlCommand("Select * from Warehouses where warehouse=@warehouse", myConnection);
+                myConnection.Open();
+                myCommand.Parameters.AddWithValue("@warehouse", e.Node.Text);
+                myConnection.Close();
+                displayData3();
+                myCommand.Connection.Open();
+                SqlDataReader myreader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                if (myreader.Read() == true)
+                {
+                    textBox1.Text = myreader["id_warehouse"].ToString();
+                    textBox2.Text = myreader["warehouse"].ToString();
+                    textBox3.Text = myreader["address"].ToString();
+                    textBox4.Text = myreader["zip_code"].ToString();
+                }
+                if (myConnection.State == ConnectionState.Open)
+                {
+                    myConnection.Dispose();
+                }
+
+
+                groupBox2.Visible = true;
+                myConnection = new SqlConnection(frm.cs);
+                myCommand = new SqlCommand("Select * from Inventory where product_name=@product_name", myConnection);
+                myConnection.Open();
+                myCommand.Parameters.AddWithValue("@product_name", e.Node.Text);
+                myConnection.Close();
+                displayData4();
+                myCommand.Connection.Open();
+                myreader = myCommand.ExecuteReader(CommandBehavior.CloseConnection);
+                if (myreader.Read() == true)
+                {
+                    textBox5.Text = myreader["id_product"].ToString();
+                    textBox6.Text = myreader["warehouse"].ToString();
+                    textBox7.Text = myreader["product_name"].ToString();
+                    textBox8.Text = myreader["delivery_price"].ToString();
+                    textBox9.Text = myreader["sale_price"].ToString();
+                    textBox10.Text = myreader["measure"].ToString();
+                    comboBox1.Text = myreader["supplier"].ToString();
+                }
+                if (myConnection.State == ConnectionState.Open)
+                {
+                    myConnection.Dispose();
+                }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -104,7 +188,7 @@ namespace Management_System
                 MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
 
-            TreeNode node = new TreeNode(textBox1.Text);
+            TreeNode node = new TreeNode(textBox2.Text);
             treeView1.Nodes.Add(node);
 
             textBox1.Clear();
@@ -113,6 +197,53 @@ namespace Management_System
             textBox4.Clear();
             
             groupBox1.Visible = false;
+        }
+
+        private void новаСтокаToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            groupBox2.Visible = true;
+            textBox5.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myConnection = new SqlConnection(frm.cs);
+                myCommand = new SqlCommand("insert into Inventory(id_product, product_name, delivery_price, sale_price, measure, supplier, warehouse) values(@id_product, @product_name, @delivery_price, @sale_price, @measure, @supplier, @warehouse)", myConnection);
+                myConnection.Open();
+                myCommand.Parameters.AddWithValue("@id_product", textBox5.Text);
+                myCommand.Parameters.AddWithValue("@warehouse", textBox6.Text);
+                myCommand.Parameters.AddWithValue("@product_name", textBox7.Text);
+                myCommand.Parameters.AddWithValue("@delivery_price", textBox8.Text);
+                myCommand.Parameters.AddWithValue("@sale_price", textBox9.Text);
+                myCommand.Parameters.AddWithValue("@measure", textBox10.Text);
+                myCommand.Parameters.AddWithValue("@supplier", comboBox1.Text);
+                myCommand.ExecuteNonQuery();
+                myConnection.Close();
+                MessageBox.Show("Успешно въведена нова стока!");
+                if (myConnection.State == ConnectionState.Open)
+                {
+                    myConnection.Dispose();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            TreeNode node = new TreeNode(textBox7.Text);
+            TreeNode childnode = treeView1.Nodes[0];
+            node.Nodes.Add(node);
+
+            textBox5.Clear();
+            textBox6.Clear();
+            textBox7.Clear();
+            textBox8.Clear();
+            textBox9.Clear();
+            textBox10.Clear();
+
+            groupBox2.Visible = false;
         }
     }
 }
